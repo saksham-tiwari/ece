32e13f40fb68aa14e670c340638a6879e03f4b81
saksham-tiwari <lucky121saty@gmail.com>
2022-06-15 21:39:58 +0530

    Updates

---
+++ precache-manifest.5488bcbc8de71c57ce8733c41058c817.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "ffa2c661810b188f5e39",
+    "url": "/ece/static/css/main.5917c700.chunk.css"
+  },
+  {
+    "revision": "ffa2c661810b188f5e39",
+    "url": "/ece/static/js/main.ffa2c661.chunk.js"
+  },
+  {
+    "revision": "cc79fa3910fc97852656",
+    "url": "/ece/static/js/1.cc79fa39.chunk.js"
+  },
+  {
+    "revision": "b173162c6ecfdc553864",
+    "url": "/ece/static/js/2.b173162c.chunk.js"
+  },
+  {
+    "revision": "a9651cde5ba3680607b7",
+    "url": "/ece/static/js/runtime~main.a9651cde.js"
+  },
+  {
+    "revision": "4bacf68044c8507d01efdc84e8d56654",
+    "url": "/ece/static/media/glimpses.4bacf680.png"
+  },
+  {
+    "revision": "48a4910050a68ec7667d1de029004ced",
+    "url": "/ece/static/media/about.48a49100.png"
+  },
+  {
+    "revision": "7ebb0928c09c1c1de1ed156144cb75b4",
+    "url": "/ece/static/media/akg.7ebb0928.png"
+  },
+  {
+    "revision": "21b2d38e41aeeb132fdf98de8ca75559",
+    "url": "/ece/static/media/1.21b2d38e.png"
+  },
+  {
+    "revision": "62de6a88cfeb38c07834d2ad4633d69d",
+    "url": "/ece/static/media/2.62de6a88.png"
+  },
+  {
+    "revision": "ab79492d339d45693ddc4216d1c2751c",
+    "url": "/ece/static/media/3.ab79492d.png"
+  },
+  {
+    "revision": "c1b90043a84f583b7354ec00da875832",
+    "url": "/ece/static/media/4.c1b90043.png"
+  },
+  {
+    "revision": "a397ab5b3ca180aafe67cb431773b28a",
+    "url": "/ece/static/media/5.a397ab5b.png"
+  },
+  {
+    "revision": "e5c4cefe12523ed145ee15c0f0f41bbb",
+    "url": "/ece/static/media/e1.e5c4cefe.png"
+  },
+  {
+    "revision": "7720ae105e16a40286aaf9d5f8fdbfb9",
+    "url": "/ece/static/media/e2.7720ae10.png"
+  },
+  {
+    "revision": "859e56bea73713f85ec9212ac1ebe186",
+    "url": "/ece/static/media/e3.859e56be.png"
+  },
+  {
+    "revision": "219e8423235ef1cb599a82bdcb7c940c",
+    "url": "/ece/static/media/e4.219e8423.png"
+  },
+  {
+    "revision": "af5b8da12b32c5e74532c6446eb356b4",
+    "url": "/ece/static/media/Gilroy-Regular.af5b8da1.ttf"
+  },
+  {
+    "revision": "45d934c70831a902302b2113bb8fc42d",
+    "url": "/ece/static/media/main.45d934c7.png"
+  },
+  {
+    "revision": "613ceb2bfc9c06b9c1091f1fb2746cd2",
+    "url": "/ece/index.html"
+  }
+];
